c45f820f9f8f7697b79611d3671c9b50330c7971
YazanAlshoubakii <yazan.h.alshoubaki@gmail.com>
2024-07-21 01:35:45 +0300

    Add appointments routers and update appointments using id

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,20 @@
-const express = require("express");
-require("dotenv").config();
-const cors = require("cors");
-require("./models/db");
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+require('./models/db');
 
 //routers
 const app = express();
+
+const appointmentsRouter = require('./routes/appointments');
 
 //built-in middleware
 app.use(express.json());
 app.use(cors());
 
 // router middleware
+
+app.use('/appointments', appointmentsRouter);
 
 const PORT = process.env.PORT || 5000;
 
