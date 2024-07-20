4d2bbd60d883a9e21fec163ab3fa9856e78c59e2
Bashar-zakarneh <bashar.a.zakarneh@gmail.com>
2024-07-20 23:07:09 +0300

    Starter-Code

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+require("./models/db");
+
+//routers
+const app = express();
+
+//built-in middleware
+app.use(express.json());
+app.use(cors());
+
+// router middleware
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`server on ${PORT}`);
+});
---
+++ backend/middlewares/authorization.js
@@ -0,0 +1,5 @@
+const pool = require("../models/db");
+
+const authorization = (string) => {};
+
+module.exports = authorization;
