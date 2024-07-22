18e77f1d9f70ebeeaea286fca9a98b03ce455e1e
YazanAlshoubakii <yazan.h.alshoubaki@gmail.com>
2024-07-22 23:08:22 +0300

    Send Prescription Request Function

--- backend/controllers/prescription.js
+++ backend/controllers/prescription.js
@@ -23,4 +23,39 @@ const getAllPrescription = (req, res) => {
     });
 };
 
-module.exports = { getAllPrescription };
+const SendPrescriptionRequest = (req, res) => {
+  const { doctor_id, user_id, pharmacist_id, title, description, quantity } =
+    req.body;
+
+  const query = `INSERT INTO prescription
+   (doctor_id, user_id, pharmacist_id, title, description, quantity)
+    VALUES ($1,$2,$3,$4,$5,$6) RETURNING *;`;
+
+  const data = [
+    doctor_id,
+    user_id,
+    pharmacist_id,
+    title,
+    description,
+    quantity,
+  ];
+
+  pool
+    .query(query, data)
+    .then((result) => {
+      res.status(200).json({
+        success: true,
+        message: 'Prescription created successfully',
+        result: result.rows[0],
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: 'Server error',
+        err: err.message,
+      });
+    });
+};
+
+module.exports = { getAllPrescription, SendPrescriptionRequest };
