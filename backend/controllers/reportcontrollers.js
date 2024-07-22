074b53cf68219d291c9f1a049954c17b7c65ce04
BasheerBino <basheer.bino@gmail.com>
2024-07-22 23:31:46 +0300

    getReportsByNationalId

--- backend/controllers/reportcontrollers.js
+++ backend/controllers/reportcontrollers.js
@@ -1,59 +1,81 @@
+const pool = require("../models/db");
 
-const pool=require("../models/db")
+const getAllReports = (req, res) => {
+  // const quer=`SELECT * FROM medical_reports WHERE user_id AND doctor_id `
+  pool
+    .query(`SELECT * FROM medical_reports `)
 
-const getAllReports=(req, res)=>{
-// const quer=`SELECT * FROM medical_reports WHERE user_id AND doctor_id `
-pool
-.query(`SELECT * FROM medical_reports `)
-
-.then((result)=>{
-    res.status(200).json({
-        success:true,
-        message:"All The Reports",
-        result:result.rows
+    .then((result) => {
+      res.status(200).json({
+        success: true,
+        message: "All The Reports",
+        result: result.rows,
+      });
     })
-})
-.catch((error)=>{
-    res.status(500).json({
-        success:false,
-        message:"Server Error",
-        error:error.message
-    })
-    console.log(error)
-})
-} 
+    .catch((error) => {
+      res.status(500).json({
+        success: false,
+        message: "Server Error",
+        error: error.message,
+      });
+      console.log(error);
+    });
+};
 
-const createMedicalReport=(req,res)=>{
-    const { user_id, doctor_id,title,description,image_url,time} = req.body;
-   
-    const query = `INSERT INTO medical_reports (user_id, doctor_id,title,description,image_url,time) VALUES ($1,$2,$3,$4,$5,$6)`;
-    const data = [
-        user_id,
-        doctor_id,
-        title,
-        description,
-        image_url,
-        time,
-    ];
-    
-    pool
-      .query(query, data)
-      .then((result) => {
+const createMedicalReport = (req, res) => {
+  const { user_id, doctor_id, title, description, image_url, time } = req.body;
+
+  const query = `INSERT INTO medical_reports (user_id, doctor_id,title,description,image_url,time) VALUES ($1,$2,$3,$4,$5,$6)`;
+  const data = [user_id, doctor_id, title, description, image_url, time];
+
+  pool
+    .query(query, data)
+    .then((result) => {
+      res.status(200).json({
+        success: true,
+        message: "report created successfully",
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: "Server Error",
+        err: err.message,
+      });
+    });
+};
+
+const getReportsByNationalId = (req, res) => {
+  const id = req.params.national_id;
+  console.log(id);
+  const query = `SELECT users.first_name,users.last_name,national_id,medical_reports.* 
+  FROM medical_reports
+  INNER JOIN users ON users.id=medical_reports.user_id WHERE users.national_id=($1)`;
+  const data = [id];
+
+  pool
+    .query(query, data)
+    .then((result) => {
+      if (result.rows.length !== 0) {
         res.status(200).json({
           success: true,
-          message: 'report created successfully',
+          message: `The reports with national_id: ${id}`,
+          result: result.rows,
         });
-      })
-      .catch((err) => {
-        res.status(500).json({
-          success: false,
-          message: 'Server Error',
-          err:err.message
-        });
+      } else {
+        throw new Error("Error happened while getting reports");
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: "Server error",
+        err: err,
       });
-}
-
-module.exports={
-    getAllReports,
-    createMedicalReport
+    });
+};
+module.exports = {
+  getAllReports,
+  createMedicalReport,
+  getReportsByNationalId,
 };
