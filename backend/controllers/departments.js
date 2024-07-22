bfc52779708ded8f4e820255d76e2af5e14bddef
BasheerBino <basheer.bino@gmail.com>
2024-07-22 21:28:57 +0300

    addNewDepartment function and router

---
+++ backend/controllers/departments.js
@@ -0,0 +1,34 @@
+const pool =require("../models/db")
+
+
+const addNewDepartment =  (req, res) => {
+    const { name, description } = req.body;
+   
+    const query = `INSERT INTO departments (name, description) VALUES ($1,$2)`;
+    const data = [
+      name,
+      description,
+    ];
+    pool
+      .query(query, data)
+      .then((result) => {
+        res.status(200).json({
+          success: true,
+          message: 'Department added successfully',
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          success: false,
+          message: 'Server Error',
+          err:err.message
+        });
+      });
+  };
+
+
+
+
+  module.exports={
+    addNewDepartment
+  }
