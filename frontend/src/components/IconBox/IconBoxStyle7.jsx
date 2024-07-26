f44b9cf1385363ad664edbdb62844c5e3457a29e
BasheerBino <basheer.bino@gmail.com>
2024-07-27 02:14:00 +0300

    create department component

--- frontend/src/components/IconBox/IconBoxStyle7.jsx
+++ frontend/src/components/IconBox/IconBoxStyle7.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import parser from 'html-react-parser';
 import { Link } from 'react-router-dom';
 
-export default function IconBoxStyle6({ title, subTitle, iconUrl, href }) {
+export default function IconBoxStyle6({ name, description, href }) {
   return (
     <div className="cs_iconbox cs_style_7">
       <div className="cs_iconbox_icon">
-        <img src={iconUrl} alt="Icon" />
+        <img src="https://prohealth-react.vercel.app/images/departments/icon_1.svg" alt="Icon" />
       </div>
-      <h2 className="cs_iconbox_title cs_fs_32">{parser(title)}</h2>
-      <p className="cs_iconbox_subtitle m-0">{parser(subTitle)}</p>
+      <h2 className="cs_iconbox_title cs_fs_32">{parser(name)}</h2>
+      <p className="cs_iconbox_subtitle m-0">{parser(description)}</p>
       <Link to={href} className="cs_iconbox_btn cs_center">
         <img src="images/icons/arrow_white.svg" alt="Icon" />
         <img src="images/icons/arrow_white.svg" alt="Icon" />
--- frontend/src/components/Pages/Departments.jsx
+++ frontend/src/components/Pages/Departments.jsx
@@ -1,70 +1,15 @@
-import React from 'react';
-import BannerSectionStyle5 from '../Section/BannerSection/BannerSectionStyle5';
-import Section from '../Section';
-import DepartmentSectionStyle3 from '../Section/DepartmentSection/DepartmentSectionStyle3';
-import BannerSectionStyle6 from '../Section/BannerSection/BannerSectionStyle6';
-import { pageTitle } from '../../helpers/PageTitle';
-const departmentData = [
-  {
-    iconUrl: 'images/departments/icon_1.svg',
-    title: 'Emergency Department',
-    subTitle:
-      'This department provides immediate medical care to patients with acute illnesses or injuries that require immediate attention.',
-    href: '/departments/department-details',
-  },
-  {
-    iconUrl: 'images/departments/icon_1.svg',
-    title: 'Cardiology Department',
-    subTitle:
-      'This department provides immediate medical care to patients with acute illnesses or injuries that require immediate attention.',
-    href: '/departments/department-details',
-  },
-  {
-    iconUrl: 'images/departments/icon_1.svg',
-    title: 'Pediatric Department',
-    subTitle:
-      'This department provides immediate medical care to patients with acute illnesses or injuries that require immediate attention.',
-    href: '/departments/department-details',
-  },
-  {
-    iconUrl: 'images/departments/icon_1.svg',
-    title: 'Obstetrics and Gynecology',
-    subTitle:
-      'This department provides immediate medical care to patients with acute illnesses or injuries that require immediate attention.',
-    href: '/departments/department-details',
-  },
-  {
-    iconUrl: 'images/departments/icon_1.svg',
-    title: 'Psychiatry Department',
-    subTitle:
-      'This department provides immediate medical care to patients with acute illnesses or injuries that require immediate attention.',
-    href: '/departments/department-details',
-  },
-  {
-    iconUrl: 'images/departments/icon_1.svg',
-    title: 'Neurology Department',
-    subTitle:
-      'This department provides immediate medical care to patients with acute illnesses or injuries that require immediate attention.',
-    href: '/departments/department-details',
-  },
-  {
-    iconUrl: 'images/departments/icon_1.svg',
-    title: 'Occupational Therapy Department',
-    subTitle:
-      'This department provides immediate medical care to patients with acute illnesses or injuries that require immediate attention.',
-    href: '/departments/department-details',
-  },
-  {
-    iconUrl: 'images/departments/icon_1.svg',
-    title: 'Physical Therapy Department',
-    subTitle:
-      'This department provides immediate medical care to patients with acute illnesses or injuries that require immediate attention.',
-    href: '/departments/department-details',
-  },
-];
+import React, { Suspense } from "react";
+import BannerSectionStyle5 from "../Section/BannerSection/BannerSectionStyle5";
+import Section from "../Section";
+import DepartmentSectionStyle3 from "../Section/DepartmentSection/DepartmentSectionStyle3";
+import BannerSectionStyle6 from "../Section/BannerSection/BannerSectionStyle6";
+import { pageTitle } from "../../helpers/PageTitle";
+import { useLoaderData, Await } from "react-router-dom";
+
 
 export default function Departments() {
-  pageTitle('Departments');
+  pageTitle("Departments");
+  const { results } = useLoaderData();
   return (
     <>
       <BannerSectionStyle5
@@ -73,8 +18,15 @@ export default function Departments() {
         title="Get to Know <br>ProHealth Departments"
         subTitle="At ProHealth, we offer a wide range of medical and healthcare services that are designed to meet your individual needs and help you achieve optimal health."
       />
+
       <Section bottomMd={140} bottomLg={100} bottomXl={60}>
-        <DepartmentSectionStyle3 data={departmentData} />
+        <Suspense fallback={<p>Loading data</p>}>
+          <Await resolve={results} errorElement={<p>error loading data</p>}>
+             {(results)=>{
+              return <DepartmentSectionStyle3 data={results.data.result} />
+             }}
+          </Await>
+        </Suspense>
       </Section>
       <Section className="cs_footer_margin_0">
         <BannerSectionStyle6
