2fc4ae730846035277ee3377e11bcd2b392b26be
Bashar-zakarneh <bashar.a.zakarneh@gmail.com>
2024-07-27 03:45:24 +0300

    Change doctors_controller

--- frontend/src/components/Pages/Doctors.jsx
+++ frontend/src/components/Pages/Doctors.jsx
@@ -6,53 +6,6 @@ import Section from '../Section';
 import { pageTitle } from '../../helpers/PageTitle';
 import { useLoaderData, Await } from "react-router-dom";
 
-// const teamData = [
-//   {
-//     imgUrl: 'https://prohealth-react.vercel.app/images/doctors/doctor_1.png',
-//     department: 'Emergency Department',
-//     name: 'Dr. Lisa Chen, MD',
-//     designation: 'Emergency Medicine Specialist',
-//     description:
-//       'Dr. Chen has over 10 years of experience in emergency medicine and is board certified in emergency medicine. She has a special interest in critical care and trauma management.',
-//     social: [
-//       { icon: 'fa6-brands:facebook-f', href: '/about' },
-//       { icon: 'fa6-brands:linkedin-in', href: '/about' },
-//       { icon: 'fa6-brands:twitter', href: '/about' },
-//     ],
-//     category: 'cardiology',
-//     href: '/doctors/doctor-details',
-//   },
-//   {
-//     imgUrl: 'https://prohealth-react.vercel.app/images/doctors/doctor_2.png',
-//     department: 'Emergency Department',
-//     name: 'Dr. Michael Johnson, MD',
-//     designation: 'Emergency Medicine Specialist',
-//     description:
-//       'Dr. Johnson is a highly experienced emergency medicine physician with over 15 years of experience. He is board-certified in emergency medicine and has a special interest in disaster management.',
-//     social: [
-//       { icon: 'fa6-brands:facebook-f', href: '/about' },
-//       { icon: 'fa6-brands:linkedin-in', href: '/about' },
-//       { icon: 'fa6-brands:twitter', href: '/about' },
-//     ],
-//     category: 'emergency',
-//     href: '/doctors/doctor-details',
-//   },
-//   {
-//     imgUrl: 'https://prohealth-react.vercel.app/images/doctors/doctor_3.png',
-//     department: 'Emergency Department',
-//     name: 'Dr. Karen Lee, MD',
-//     designation: 'Emergency Medicine Specialist',
-//     description:
-//       'Dr. Lee is a skilled emergency medicine physician with expertise in the treatment of acute medical conditions. She is board-certified in emergency medicine and has a special interest in cardiac.',
-//     social: [
-//       { icon: 'fa6-brands:facebook-f', href: '/about' },
-//       { icon: 'fa6-brands:linkedin-in', href: '/about' },
-//       { icon: 'fa6-brands:twitter', href: '/about' },
-//     ],
-//     category: 'pediatric',
-//     href: '/doctors/doctor-details',
-//   },
-// ];
 
 export default function Doctors() {
   pageTitle('Doctors');
