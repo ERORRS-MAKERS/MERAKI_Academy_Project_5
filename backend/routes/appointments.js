60aebc658405739bda11b6a96a75179914e8e19e
YazanAlshoubakii <yazan.h.alshoubaki@gmail.com>
2024-07-31 21:07:44 +0300

    Add endPoint getAppointmentsByUser

--- backend/routes/appointments.js
+++ backend/routes/appointments.js
@@ -4,11 +4,13 @@ const {
   updateAppointmentById,
   getAllAppointments,
   addNewAppointments,
+  getAllAppointmentsByUserId,
 } = require('../controllers/appointments');
 
 const appointmentsRouter = express.Router();
 
 appointmentsRouter.put('/:id', updateAppointmentById);
+appointmentsRouter.get('/:id', getAllAppointmentsByUserId);
 appointmentsRouter.get('/', getAllAppointments);
 appointmentsRouter.post('/', addNewAppointments);
 module.exports = appointmentsRouter;
