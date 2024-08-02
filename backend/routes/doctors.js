edbbb1f71d44357048c3febd438e51f2d8c5b7d2
Bashar-zakarneh <bashar.a.zakarneh@gmail.com>
2024-08-02 17:40:45 +0300

    Add router to addNewpharmacist end point

--- backend/routes/doctors.js
+++ backend/routes/doctors.js
@@ -1,18 +1,20 @@
-const express = require('express');
+const express = require("express");
 const {
   addNewDoctor,
   getAllDoctors,
   updateDoctorById,
   deleteDoctorById,
-  getDoctorsByStatusOfHiring
-} = require('../controllers/doctors');
+  getDoctorsByStatusOfHiring,
+  addNewpharmacist,
+} = require("../controllers/doctors");
 
 const doctorsRouter = express.Router();
 
-doctorsRouter.post('/', addNewDoctor);
-doctorsRouter.get('/', getAllDoctors);
-doctorsRouter.put('/:id', updateDoctorById);
-doctorsRouter.delete('/:id', deleteDoctorById);
-doctorsRouter.get('/hiring_status',getDoctorsByStatusOfHiring)
+doctorsRouter.post("/", addNewDoctor);
+doctorsRouter.get("/", getAllDoctors);
+doctorsRouter.put("/:id", updateDoctorById);
+doctorsRouter.delete("/:id", deleteDoctorById);
+doctorsRouter.get("/hiring_status", getDoctorsByStatusOfHiring);
+doctorsRouter.post("/add/pharmacist", addNewpharmacist);
 
 module.exports = doctorsRouter;
