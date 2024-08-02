7d3dd8947b901abf8a903008443f5dd827f5e13c
Bashar-zakarneh <bashar.a.zakarneh@gmail.com>
2024-08-02 17:54:51 +0300

    Add pharmacistLogin end point & router

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -153,8 +153,65 @@ const doctorLogin = (req, res) => {
     });
 };
 
+const pharmacistLogin = (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "Email and password are required",
+    });
+  }
+  const query = `SELECT * FROM pharmacy WHERE email = $1`;
+  const data = [email.toLowerCase()];
+
+  pool
+    .query(query, data)
+    .then((result) => {
+      if (result.rows.length) {
+        bcrypt.compare(password, result.rows[0].password, (err, response) => {
+          if (err) res.json(err);
+          if (response) {
+            const payload = {
+              userId: result.rows[0].id,
+              country: result.rows[0].country,
+              role: result.rows[0].role_id,
+            };
+            const options = { expiresIn: "1d" };
+            const secret = process.env.SECRET;
+            const token = jwt.sign(payload, secret, options);
+            if (token) {
+              return res.status(200).json({
+                token,
+                success: true,
+                message: `Valid login credentials`,
+                userId: result.rows[0].id,
+              });
+            } else {
+              throw Error;
+            }
+          } else {
+            res.status(403).json({
+              success: false,
+              message: `The email doesn’t exist or the password you’ve entered is incorrect`,
+            });
+          }
+        });
+      } else throw Error;
+    })
+    .catch((err) => {
+      res.status(403).json({
+        success: false,
+        message:
+          "The email doesn’t exist or the password you’ve entered is incorrect",
+        err,
+      });
+    });
+};
+
 module.exports = {
   register,
   login,
   doctorLogin,
+  pharmacistLogin,
 };
