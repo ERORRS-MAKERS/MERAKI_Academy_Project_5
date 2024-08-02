f36d1660ceadd39c4dc3c496097c7db5cc385525
Bashar-zakarneh <bashar.a.zakarneh@gmail.com>
2024-08-02 18:22:03 +0300

    Add pharmacistLogin page

---
+++ frontend/src/components/Pages/PharmacistLogin.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import pharmacistLoginSection from '../Section/LoginSection/pharmacistLoginSection'
+
+const PharmacistLogin = () => {
+  return (
+    <pharmacistLoginSection />
+  )
+}
+
+export default PharmacistLogin
