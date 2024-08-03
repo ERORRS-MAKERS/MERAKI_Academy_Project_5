6627d822f81c5b84c0dd01ac92f52dc32738b57b
Bashar-zakarneh <bashar.a.zakarneh@gmail.com>
2024-08-03 21:05:40 +0300

    Add doctorReducer to reduxStore

--- frontend/src/service/redux/store.js
+++ frontend/src/service/redux/store.js
@@ -1,19 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import authReducer from './reducers/auth/index';
-import prescriptionsReducer from './reducers/prescriptions/index';
-import appointmentsReducer from './reducers/AppointmentsTableUser/index';
-import hiringRequestReducer from './reducers/doctorsHiringRequest/index'
+import authReducer from "./reducers/auth/index";
+import prescriptionsReducer from "./reducers/prescriptions/index";
+import appointmentsReducer from "./reducers/AppointmentsTableUser/index";
+import hiringRequestReducer from "./reducers/doctorsHiringRequest/index";
 import pharmacistReducer from "./reducers/pharmacist/index";
-
+import doctorReducer from "./reducers/doctorsAuth/index";
 
 export default configureStore({
   reducer: {
     auth: authReducer,
     prescriptions: prescriptionsReducer,
-    appointments: appointmentsReducer, 
-    hiringRequest:hiringRequestReducer,
+    appointments: appointmentsReducer,
+    hiringRequest: hiringRequestReducer,
     pharmacist: pharmacistReducer,
-
+    doctor: doctorReducer,
   },
 });
