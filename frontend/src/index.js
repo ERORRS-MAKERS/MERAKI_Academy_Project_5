9f8498b164e053095493401a1f4713ba4982d491
Bashar-zakarneh <bashar.a.zakarneh@gmail.com>
2024-08-08 17:05:12 +0300

    install package and wrap app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { Provider } from 'react-redux';
-import store from './service/redux/store';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'slick-carousel/slick/slick.css';
-import './sass/index.scss';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./service/redux/store";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "slick-carousel/slick/slick.css";
+import "./sass/index.scss";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+const clientId =
+  "635113684331-8366vub6b4vggccnhvbo9m69dubkrid0.apps.googleusercontent.com";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <GoogleOAuthProvider clientId={clientId}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
