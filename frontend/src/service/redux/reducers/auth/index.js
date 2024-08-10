8606c7f9bc1cc45c6046e0c68c880f32f41c6926
Bashar-zakarneh <bashar.a.zakarneh@gmail.com>
2024-08-10 05:16:32 +0300

    Add role in authSlice

--- frontend/src/service/redux/reducers/auth/index.js
+++ frontend/src/service/redux/reducers/auth/index.js
@@ -5,17 +5,21 @@ export const authSlice = createSlice({
   initialState: {
     token: localStorage.getItem("token") || null,
     userId: localStorage.getItem("userId") || null,
+    role: localStorage.getItem("role") || null,
     isLoggedIn: false,
     showNotification: false,
-    patientId:''
+    patientId: "",
   },
 
   reducers: {
     setLogin: (state, action) => {
       localStorage.setItem("token", action.payload.token);
       localStorage.setItem("userId", action.payload.userId);
+      localStorage.setItem("role", action.payload.role);
 
       state.token = action.payload.token;
+      state.userId = action.payload.userId;
+      state.role = action.payload.role;
       state.isLoggedIn = true;
       // state.isLoggedIn = action.payload.success;
     },
@@ -27,22 +31,23 @@ export const authSlice = createSlice({
     setLogout: (state) => {
       state.token = null;
       state.userId = null;
+      state.role = null;
       state.isLoggedIn = false;
       localStorage.clear();
-      state.showNotification = false
+      state.showNotification = false;
     },
     setNotification: (state) => {
       state.showNotification = !state.showNotification;
-      console.log(state.showNotification)
+      console.log(state.showNotification);
     },
-    setPatientId: (state,action) => {
+    setPatientId: (state, action) => {
       state.patientId = action.payload;
-      console.log(state.patientId)
+      console.log(state.patientId);
     },
   },
 });
 
-export const { setLogin, setLogout, setUserId, setNotification,setPatientId } =
+export const { setLogin, setLogout, setUserId, setNotification, setPatientId } =
   authSlice.actions;
 
 export default authSlice.reducer;
