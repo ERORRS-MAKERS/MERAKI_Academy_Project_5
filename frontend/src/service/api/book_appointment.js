fe6c9c77614f631b63c86401b99cb4b98c29e9d4
YazanAlshoubakii <yazan.h.alshoubaki@gmail.com>
2024-08-10 04:24:39 +0300

    some styles

--- frontend/src/service/api/book_appointment.js
+++ frontend/src/service/api/book_appointment.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 import { SendEmail } from './EmailJsApi';
 
-
-export const bookAppointment = async (user_id, department_id, time, notes,department_name) => {
+export const bookAppointment = async (
+  user_id,
+  department_id,
+  time,
+  notes,
+  department_name
+) => {
   try {
     const response = await axios.post('http://127.0.0.1:5000/appointments/', {
       user_id,
@@ -10,13 +15,15 @@ export const bookAppointment = async (user_id, department_id, time, notes,depart
       time,
       notes,
     });
-   const userDetails = await axios.get(`http://127.0.0.1:5000/users/${user_id}`) 
-   let name = userDetails.data.result.first_name
-    let email=userDetails.data.result.email
-  
-  SendEmail(department_name,time,name,email)
-  return response.data;
 
+    const userDetails = await axios.get(
+      `http://127.0.0.1:5000/users/${user_id}`
+    );
+    let name = userDetails.data.result.first_name;
+    let email = userDetails.data.result.email;
+
+    SendEmail(department_name, time, name, email);
+    return response.data;
   } catch (error) {
     throw error.response.data;
   }
