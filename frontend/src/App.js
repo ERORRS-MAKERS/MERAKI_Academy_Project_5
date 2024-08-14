ace8b413c82fe81a073702a4c7cdbad2312f3ad0
Bashar-zakarneh <bashar.a.zakarneh@gmail.com>
2024-08-14 05:04:31 +0300

    Handle login with refreshing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,22 @@
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes/router';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/router";
+import { setLogin } from "./service/redux/reducers/auth";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const userId = localStorage.getItem("userId");
+    const role = localStorage.getItem("role");
+
+    if (token && userId && role) {
+      dispatch(setLogin({ token, userId, role }));
+    }
+  }, [dispatch]);
+
   return <RouterProvider router={router} />;
 }
 
